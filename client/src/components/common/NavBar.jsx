09d19b958fde585d9699e1aa6fbf10b9aba77cf8
박예찬 <jkyyc3@gmail.com>
2021-11-27 00:06:51 +0900

    [Chore] Set buildspec.yml
    invalidation 작성

--- client/src/components/common/NavBar.jsx
+++ client/src/components/common/NavBar.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { BorderBtn } from '../../styles/common';
 
 export default function NavBar() {
-  return <BorderBtn></BorderBtn>;
+  return <BorderBtn>여기 테스트</BorderBtn>;
 }
