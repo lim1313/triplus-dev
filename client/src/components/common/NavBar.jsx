824abcb784c0e202b6eed3132fa05c72a621a354
박예찬 <jkyyc3@gmail.com>
2021-11-27 01:32:43 +0900

    [Chore] Error while executing command/6
    무효화 확인 테스트!

--- client/src/components/common/NavBar.jsx
+++ client/src/components/common/NavBar.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { BorderBtn } from '../../styles/common';
 
 export default function NavBar() {
-  return <BorderBtn>여기 테스트</BorderBtn>;
+  return <BorderBtn>자동화 배포 테스트!!</BorderBtn>;
 }
