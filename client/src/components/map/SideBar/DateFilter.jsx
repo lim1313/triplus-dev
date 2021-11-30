53bad84e8cf70e1c9786bc4594074ab934377abb
yeji <shfks1212@naver.com>
2021-11-30 13:53:55 +0900

    [Feat] Create datePicker
    날짜선택 기능 추가

--- client/src/components/map/SideBar/DateFilter.jsx
+++ client/src/components/map/SideBar/DateFilter.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/esm/locale';
 import { FilterFrame } from '../../../styles/map/filterFrame';
+import 'react-datepicker/dist/react-datepicker.css';
+import styled from 'styled-components';
 
-export default function DateFilter() {
+const FilterWrapper = styled(FilterFrame)`
+  overflow: hidden;
+`;
+
+const DateFrame = styled(DatePicker)`
+  border: none;
+  text-align: center;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+function App() {
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+
+  const handleDateChangeRaw = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <FilterFrame>
-      <span>날짜선택</span>
-    </FilterFrame>
+    <FilterWrapper>
+      <DateFrame
+        selectsRange={true}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(update) => {
+          setDateRange(update);
+        }}
+        placeholderText='여행날짜를 선택하세요'
+        locale={ko}
+        dateFormat='yyyy년 MM월 dd일'
+        minDate={new Date()}
+        showDisabledMonthNavigation
+        onChangeRaw={handleDateChangeRaw}
+      />
+    </FilterWrapper>
   );
 }
+
+export default App;
