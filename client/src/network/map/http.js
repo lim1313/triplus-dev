51ae26d7ff5790d70752152e6d455deefa19f7e9
yeji <shfks1212@naver.com>
2021-12-02 15:12:17 +0900

    [Feat] Create reservation guide card
    가이드 카드 신청 post 요청 구현

--- client/src/network/map/http.js
+++ client/src/network/map/http.js
@@ -6,13 +6,20 @@ axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 //! 400, 500 에러, 오류 처리 필수
-//TODO 범위 내의 카드 가져오기
+//TODO GET 범위 내의 카드 가져오기
 export const getGuideCards = async (params) => {
   const res = await axios.get(`${http}/map`, { params });
   return res.data.guideCardList;
 };
 
+//TODO GET 범위 내의 카드 모달
 export const getCardModal = async (params) => {
   const res = await axios.get(`${http}/map/guide-card`, { params: { 'guide-id': params } });
   return res.data.guideCardList;
+};
+
+// TODO POST 가이드 신청하기
+export const rezGuide = async (guideId) => {
+  const res = await axios.post(`${http}/map`, { guideId });
+  return res;
 };
