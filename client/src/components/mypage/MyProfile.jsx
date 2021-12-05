efe557d25df60f8171802333cce42dd08335dec7
yeji <shfks1212@naver.com>
2021-12-05 16:36:36 +0900

    [Style] Create mypage
    마이페이지 전체적인 레이아웃 구현

---
+++ client/src/components/mypage/MyProfile.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BorderBtn, ColorBtn } from '../../styles/common';
+
+const ProfileWrapper = styled.div`
+  margin-right: 2rem;
+`;
+
+const ImgWrapper = styled.img`
+  position: relative;
+  width: 170px;
+  height: 220px;
+  object-fit: contain;
+  border-radius: 0.5rem;
+  background-color: #fff;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem 0;
+`;
+
+const BtnColor = styled(ColorBtn)`
+  padding: 0.1em 1.1em;
+  margin-bottom: 0.5rem;
+`;
+
+const BtnBorder = styled(BorderBtn)`
+  padding: 0.1em 1.1em;
+`;
+
+export default function MyProfile() {
+  return (
+    <ProfileWrapper>
+      <ImgWrapper src='/asset/logo/logo.png' alt='프로필' />
+      <BtnWrapper>
+        <BtnColor palette='blue'>업로드</BtnColor>
+        <BtnBorder>삭제</BtnBorder>
+      </BtnWrapper>
+    </ProfileWrapper>
+  );
+}
