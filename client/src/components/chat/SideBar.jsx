80235c53b3dab04eb885e31db902c49fe0116623
박예찬 <jkyyc3@gmail.com>
2021-12-07 04:09:55 +0900

    [Feat] Create sendMessage event
    sendMessage socket.io 이벤트를 생성

--- client/src/components/chat/SideBar.jsx
+++ client/src/components/chat/SideBar.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import ChatList from './ChatList';
+import { BorderBtn } from '../../styles/common/index';
 
 const SideContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 13rem;
-  height: 100%;
+  height: inherit;
   border: 3px solid black;
   padding: 2rem;
 `;
 
-export default function SideBar() {
+const ChatList = styled(BorderBtn)`
+  font-size: 1rem;
+  border-radius: 15px;
+  margin-bottom: 1rem;
+  &:hover {
+    background-color: ${({ theme }) => theme.color.blue};
+    color: #fff;
+    border-color: ${({ theme }) => theme.color.blue};
+  }
+`;
+
+export default function SideBar({ selectRoomHandler, setUserId }) {
+  const clickRoomHandler = (e) => {
+    selectRoomHandler(e.target.id);
+  };
+
+  const dummyHandler = (e) => {
+    selectRoomHandler(e.target.id);
+    setUserId('Jortier');
+  };
+
   return (
     <SideContainer>
-      <ChatList></ChatList>
-      <ChatList></ChatList>
-      <ChatList></ChatList>
+      <ChatList id='gogo' onClick={clickRoomHandler} palette='gray'>
+        gogo
+      </ChatList>
+      <ChatList id='gogo' onClick={dummyHandler} palette='gray'>
+        toto
+      </ChatList>
+      <ChatList id='lala' onClick={clickRoomHandler} palette='gray'>
+        lala
+      </ChatList>
     </SideContainer>
   );
 }
--- client/src/pages/ChattingPage.jsx
+++ client/src/pages/ChattingPage.jsx
@@ -1,35 +1,93 @@
-import React, { useRef, useEffect } from 'react';
+/* eslint-disable no-unused-vars*/
+import React, { useState, useRef, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 
 import Loading from '../components/common/Loading';
 import ChatContainer from '../components/chat/ChatContainer';
 
+dayjs.locale('ko');
+
 export default function ChattingPage() {
   const isLoading = false;
   const socketRef = useRef();
+  const [userId, setUserId] = useState('Je-chan');
+  const [selectedRoom, setSelectedRoom] = useState('gogo');
+  const [chatBubble, setChatBubble] = useState([]);
 
+  const dateConversion = (date) => {
+    const day = dayjs(date).format('YYYY년 M월 D일');
+    const time = dayjs(date).format('A hh시 mm분');
+
+    return { day, time };
+  };
+
+  // ? send, getMessage 함수 목록
+  const myChatBubble = (DBform) => {
+    const { day, time } = dateConversion(DBform.date);
+    const { user_id: userId, content } = DBform;
+    const upState = {
+      day,
+      time,
+      userId,
+      content,
+    };
+
+    setChatBubble([...chatBubble, upState]);
+  };
+
+  //* 채팅 페이지 초기 렌더링
+  // TODO 1. Socket 연결하기
   useEffect(() => {
     socketRef.current = io.connect(`${process.env.REACT_APP_HTTPSURL}`, {
       transports: ['websocket'],
     });
-    console.log(io.connect(`${process.env.REACT_APP_HTTPSURL}`));
     console.log(socketRef.current);
   }, []);
 
+  // TODO 2. getMessage
   useEffect(() => {
-    socketRef.current.on('sendMessage', (data) => {});
-  }, []);
+    socketRef.current.on('getMessage', (data) => {
+      console.log(data);
+      myChatBubble(data);
+    });
+  });
 
-  const sendMessageHandler = (e, msg) => {
+  // * 방에 입장
+  const selectRoomHandler = (selectedRoom) => {
+    console.log(selectedRoom);
+    socketRef.current.emit('enterRoom', selectedRoom);
+  };
+
+  // * 문자 보내기
+  // ? socket 이벤트
+  const sendMessageHandler = (e, msg, userId, selectedRoom) => {
     e.preventDefault();
-    socketRef.current.emit('sendMessage', msg, () => {
-      console.log(msg);
+    const date = dayjs().format('YYYY.MM.DD hh:mm:ss:SSS');
+    const DBform = {
+      date,
+      user_id: userId,
+      content: msg,
+    };
+
+    socketRef.current.emit('sendMessage', DBform, selectedRoom, () => {
+      console.log(DBform);
+      myChatBubble(DBform);
     });
   };
 
   return isLoading ? (
     <Loading></Loading>
   ) : (
-    <ChatContainer sendMessageHandler={sendMessageHandler}></ChatContainer>
+    <ChatContainer
+      sendMessageHandler={sendMessageHandler}
+      userId={userId}
+      chatBubble={chatBubble}
+      selectedRoom={selectedRoom}
+      selectRoomHandler={selectRoomHandler}
+      // setUserId 는 없어져야 함
+      setUserId={setUserId}
+    ></ChatContainer>
   );
 }
