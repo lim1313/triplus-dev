0034f635037dc0f64d945addd066574e87cb254d
jortier <jwon1818@daum.net>
2021-12-06 23:35:48 +0900

    [Set] create model chat
    채팅 관련 모델 생성

---
+++ server/models/chat_room.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class chat_room extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  chat_room.init({
+    roomId: {
+      type: DataTypes.INTEGER,
+      field: 'room_id'
+    },
+    message: DataTypes.TEXT('long'),
+  }, {
+    sequelize,
+    modelName: 'chat_room',
+    freezeTableName: true
+  });
+  return chat_room;
+};
