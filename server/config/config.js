744fa5ca8c6b208eaeffe161ed2d5d56a75910ce
ye chan <81739782+Je-chan@users.noreply.github.com>
2021-12-17 11:18:48 +0900

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
+    "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": "triplus",
-    "host": "127.0.0.1",
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
     "dialect": "mysql"
   },
   "test": {
