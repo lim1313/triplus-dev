49a2eaf603f443f2528effdf6f979e5309b401ea
song-code21 <wkcjf131313@gmail.com>
2021-12-20 00:55:35 +0900

    [Feat] Update ListItem d-day
    d-day추가 위해 코드 변경

--- client/src/components/tourmanagement/ListItem.jsx
+++ client/src/components/tourmanagement/ListItem.jsx
@@ -73,7 +73,9 @@ export default function ListItem({ guideInfo, handleTourCardClick }) {
     let guideDay = new Date(+year, month - 1, +day).getTime();
     let gap = guideDay - today;
     let result = Math.ceil(gap / (1000 * 60 * 60 * 24));
-    return result;
+    if (result === 0) {
+      return 'day';
+    } else return result;
   };
   return (
     <TourCardLi state={guideInfo && guideInfo.state} onClick={() => handleTourCardClick(guideInfo)}>
